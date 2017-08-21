d571b83d0fe5a4010a0cb767f6f186ed41716d47
wattanapongjack <jack@wattanapongs-MacBook-Pro.local>
2017-08-21 16:41:02 +0700

    test

--- bot01.php
+++ bot01.php
@@ -1,6 +1,6 @@
 <?php
 
-include ('line-bot-api/php/line-bot.php');
+include ('line-bot.php');
 
 $channelSecret = '4564cf17f6fb37094b3b4681261cab10';
 $access_token  = '3XZyLqGB2iDayn7DrbwzEIyouwUuNfBIOfnAayMWBo0flDU9ENfFwmjWgfZDQfw+ec1cWlpKQXgD2UQGFqxydiL/PKamS7lTS0XXJSh+x9l8ntX3Aj0sbc0fCDvSNBbAfZNuiPmqkSp08bBy7b/LgQdB04t89/1O/w1cDnyilFU=';
