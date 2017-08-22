1a40cdbfc31365d911b4cda79fcba368e11b98d7
wattanapongjack <jack@wattanapongs-MacBook-Pro.local>
2017-08-22 16:05:26 +0700

    test6

--- bot01.php
+++ bot01.php
@@ -6,7 +6,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
+
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
@@ -16,6 +16,5 @@ if (!is_null($events['events'])) {
 			echo $text;
 		}
 	}
-}
-echo $content;
+
 echo "OK";
