7b956d128b31be8b0406c95d8b9ce1b6e84c6bb9
wattanapongjack <jack@wattanapongs-MacBook-Pro.local>
2017-08-22 14:40:25 +0700

    test6

--- bot01.php
+++ bot01.php
@@ -1,26 +1,47 @@
 <?php
+$access_token = '3XZyLqGB2iDayn7DrbwzEIyouwUuNfBIOfnAayMWBo0flDU9ENfFwmjWgfZDQfw+ec1cWlpKQXgD2UQGFqxydiL/PKamS7lTS0XXJSh+x9l8ntX3Aj0sbc0fCDvSNBbAfZNuiPmqkSp08bBy7b/LgQdB04t89/1O/w1cDnyilFU=';
 
-include ('line-bot.php');
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
 
-$channelSecret = '4564cf17f6fb37094b3b4681261cab10';
-$access_token  = '3XZyLqGB2iDayn7DrbwzEIyouwUuNfBIOfnAayMWBo0flDU9ENfFwmjWgfZDQfw+ec1cWlpKQXgD2UQGFqxydiL/PKamS7lTS0XXJSh+x9l8ntX3Aj0sbc0fCDvSNBbAfZNuiPmqkSp08bBy7b/LgQdB04t89/1O/w1cDnyilFU=';
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
-$bot = new BOT_API($channelSecret, $access_token);
-	
-if (!empty($bot->isEvents)) {
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-	// if ($bot->message->type == 'text') {	
-        $bot->replyMessageNew($bot->replyToken, json_encode($bot->events, JSON_UNESCAPED_UNICODE));
-    // }
-    // $bot->sendMessageNew('U203ddb078917d926c57b729b80fcd161', 'สวัสดี');
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
 
-    if ($bot->isSuccess()) {
-        echo 'Succeeded!';
-        exit();
-    }
-
-    // Failed
-    echo $bot->response->getHTTPStatus . ' ' . $bot->response->getRawBody(); 
-    exit();
-
+			echo $result . "\r\n";
+		}
+	}
 }
+echo "OK";
--- example/chapter-01.php
+++ example/chapter-01.php
@@ -1,15 +1,18 @@
 <?php
 
-include ('line-bot-api/php/line-bot.php');
+include ('line-bot.php');
 
 $channelSecret = '4564cf17f6fb37094b3b4681261cab10';
-$access_token  = 'BT57H8u2tbx1NEwXaZ9a4JayoEaq59372ZHfLqg2oWYF2WwgxjJcJ51kh9zfSKqpec1cWlpKQXgD2UQGFqxydiL/PKamS7lTS0XXJSh+x9kiNkQWOfY4Dj86TyI4wBmYOhAQk6/eI33YSVudq0TfxQdB04t89/1O/w1cDnyilFU=';
+$access_token  = '3XZyLqGB2iDayn7DrbwzEIyouwUuNfBIOfnAayMWBo0flDU9ENfFwmjWgfZDQfw+ec1cWlpKQXgD2UQGFqxydiL/PKamS7lTS0XXJSh+x9l8ntX3Aj0sbc0fCDvSNBbAfZNuiPmqkSp08bBy7b/LgQdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
 if (!empty($bot->isEvents)) {
-		
-    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+
+	// if ($bot->message->type == 'text') {	
+        $bot->replyMessageNew($bot->replyToken, json_encode($bot->events, JSON_UNESCAPED_UNICODE));
+    // }
+    // $bot->sendMessageNew('U203ddb078917d926c57b729b80fcd161', 'สวัสดี');
 
     if ($bot->isSuccess()) {
         echo 'Succeeded!';
