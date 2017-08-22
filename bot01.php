82eb9985a2dac498c5eb7498922755b37d81e95e
wattanapongjack <jack@wattanapongs-MacBook-Pro.local>
2017-08-22 14:49:55 +0700

    test6

--- bot01.php
+++ bot01.php
@@ -17,18 +17,16 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-            if ($text == "!test") {
-                $messages = [
-                    'type' => 'text',
-                    'text' => 'OK'
-                ];
-            }
+            $messages = [
+                'type' => 'text',
+                'text' => $text
+            ];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => $messages,
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
