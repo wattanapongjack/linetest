63271e80a6f2a80ffa2511265191f8b677fcc706
wattanapongjack <jack@wattanapongs-MacBook-Pro.local>
2017-08-22 15:23:23 +0700

    test6

--- line-bot.php
+++ line-bot.php
@@ -96,6 +96,10 @@ class BOT_API extends LINEBot {
             'messages'   => $messageBuilder->buildMessage()
         ]);
     }
+
+    public function getMessageContent($messageId = null){
+        return $this->httpClient->get($this->endpointBase . '/v2/bot/message/' . urlencode($messageId) . '/content');
+    }
 	
     public function isSuccess () {
         return !empty($this->response->isSucceeded()) ? true : false;
