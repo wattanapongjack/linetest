ff6e2ecf6d2ef7a6af06167c1ca1f2cf88bc817e
wattanapongjack <jack@wattanapongs-MacBook-Pro.local>
2017-08-22 16:12:11 +0700

    test6

--- line-bot.php
+++ line-bot.php
@@ -90,9 +90,7 @@ class BOT_API extends LINEBot {
     }
 	
     public function replyMessageNew ($replyToken = null, $message = null) {
-        $messageTxt = json_decode($message);
-        $mt = json_encode($messageTxt->text);
-        $messageBuilder = new TextMessageBuilder($mt);
+        $messageBuilder = new TextMessageBuilder($message);
         $this->response = $this->httpClient->post($this->endpointBase . '/v2/bot/message/reply', [
             'replyToken' => $replyToken,
             'messages'   => $messageBuilder->buildMessage()
