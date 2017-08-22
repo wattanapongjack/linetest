4b319ab997682825e192b3fca906ab4e63f9c796
wattanapongjack <jack@wattanapongs-MacBook-Pro.local>
2017-08-22 11:16:57 +0700

    test

--- bot01.php
+++ bot01.php
@@ -8,8 +8,8 @@ $access_token  = '3XZyLqGB2iDayn7DrbwzEIyouwUuNfBIOfnAayMWBo0flDU9ENfFwmjWgfZDQf
 $bot = new BOT_API($channelSecret, $access_token);
 	
 if (!empty($bot->isEvents)) {
-    
-	if ($bot->message->type == "text") {	
+
+	if ($bot->message->type == 'image') {	
         $bot->replyMessageNew($bot->replyToken, json_encode($bot->message, JSON_UNESCAPED_UNICODE));
     }
     // $bot->sendMessageNew('U203ddb078917d926c57b729b80fcd161', 'สวัสดี');
