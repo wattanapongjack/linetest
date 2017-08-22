d59e8adfc7d9eeb33ce19efc20d946fd5dba3c87
wattanapongjack <jack@wattanapongs-MacBook-Pro.local>
2017-08-22 12:32:46 +0700

    test5

--- line-bot.php
+++ line-bot.php
@@ -97,7 +97,7 @@ class BOT_API extends LINEBot {
         // Create the logger
         $logger = new Logger('my_logger');
         // Now add some handlers
-        $logger->pushHandler(new StreamHandler('/test.log', Logger::DEBUG));
+        $logger->pushHandler(new StreamHandler(__DIR__.'/test.log', Logger::DEBUG));
         $logger->pushHandler(new FirePHPHandler());
 
         // You can now use your logger
